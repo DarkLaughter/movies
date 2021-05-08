2168253f124700137405966a60dde4e66339dbcf
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-05-07 23:21:39 -0400

    testing layouts

--- src/components/moviecard/moviecard.js
+++ src/components/moviecard/moviecard.js
@@ -1,21 +1,28 @@
 
 import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 import Button from 'react-bootstrap/Button'
 import React from 'react'
 
 const moviecard = () => {
     return (
-        <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+        <CardGroup>
+        <Card border="light" style={{ width: '6rem' }}>
+            <Card.Img 
+                variant="top" 
+                src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+                style={{ width: '200px' }}    
+                />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+        </CardGroup>
     )
 }
 
