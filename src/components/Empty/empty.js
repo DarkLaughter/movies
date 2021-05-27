73e43a831849601ffc454b8a5d0869c9cc0fca53
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-05-26 22:16:40 -0400

    empty card

---
+++ src/components/Empty/empty.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+const empty = () => {
+    return (
+        <Card
+        bg="secondary"
+        text="secondary"
+        style={{ width: '18rem' }}
+        className="mb-2"
+      >
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title> Card Title </Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+}
+
+export default empty
